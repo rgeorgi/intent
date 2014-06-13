20b70eaceb915cbaa8984670da6477e92e98719f
Ryan Georgi <ryan@georgi.cc>
2014-06-12 18:03:27 -0700

    Add types for argparse.
    Former-commit-id: 05d87bf95475d56c1fae4bf6f68f49c38b5603c1

--- src/utils/commandline.py
+++ src/utils/commandline.py
@@ -14,3 +14,35 @@ def require_opt(option, msg, must_exist = False, must_exist_msg = 'The file "%s"
 		sys.stderr.write('ERROR: '+must_exist_msg % option)
 		errors = True
 	return errors
+
+
+class CommandLineException(Exception):
+	pass
+
+class FileNotExistsException(CommandLineException):
+	pass
+
+class DirNotExistsException(CommandLineException):
+	pass
+
+def exists(path):
+	if not os.path.exists(path):
+		raise CommandLineException('Path "%s" does not exist' % path)
+	else:
+		return path
+
+def existsfile(path):
+	if not os.path.exists(path):
+		raise CommandLineException('File "%s" does not exist.' % path)
+	elif not os.path.isfile(path):
+		raise CommandLineException('Path "%s" is not a file.' % path)
+	else:
+		return path
+	
+def existsdir(path):
+	if not os.path.exists(path):
+		raise CommandLineException('Directory "%s" does not exist.' % path)
+	if not os.path.isdir(path):
+		raise CommandLineException('Path "%s" is not a directory.' % path)
+	else:
+		return path
