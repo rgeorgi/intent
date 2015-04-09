58fe507462d15edf468931fee0d7ed97ac6a8b8c
Ryan Georgi <rgeorgi@gmail.com>
2015-04-09 17:49:20 -0500

    Added OrderedDefaultDict
    Former-commit-id: 7040ee82837aac707ff8589fd7b59d3544166ef7

--- intent/utils/dicts.py
+++ intent/utils/dicts.py
@@ -4,8 +4,8 @@ Created on Aug 26, 2013
 @author: rgeorgi
 '''
 import sys, re, unittest
-from collections import defaultdict
-from functools import reduce
+from collections import defaultdict, Callable, OrderedDict
+
 
 class CountDict(object):
 	def __init__(self):
@@ -80,7 +80,50 @@ class CountDict(object):
 		most_frequent_keys = self.most_frequent(minimum, num)
 		most_frequent_values = map(lambda key: self[key], most_frequent_keys)
 		return most_frequent_values
-			
+
+class DefaultOrderedDict(OrderedDict):
+	# Source: http://stackoverflow.com/a/6190500/562769
+	def __init__(self, default_factory=None, *a, **kw):
+		if (default_factory is not None and
+		   not isinstance(default_factory, Callable)):
+			raise TypeError('first argument must be callable')
+		OrderedDict.__init__(self, *a, **kw)
+		self.default_factory = default_factory
+
+	def __getitem__(self, key):
+		try:
+			return OrderedDict.__getitem__(self, key)
+		except KeyError:
+			return self.__missing__(key)
+
+	def __missing__(self, key):
+		if self.default_factory is None:
+			raise KeyError(key)
+		self[key] = value = self.default_factory()
+		return value
+
+	def __reduce__(self):
+		if self.default_factory is None:
+			args = tuple()
+		else:
+			args = self.default_factory,
+		return type(self), args, None, None, self.items()
+
+	def copy(self):
+		return self.__copy__()
+
+	def __copy__(self):
+		return type(self)(self.default_factory, self)
+
+	def __deepcopy__(self, memo):
+		import copy
+		return type(self)(self.default_factory,
+						  copy.deepcopy(self.items()))
+
+	def __repr__(self):
+		return 'OrderedDefaultDict(%s, %s)' % (self.default_factory,
+											   OrderedDict.__repr__(self))
+	
 class TwoLevelCountDict(object):
 	def __init__(self):
 		self._dict = defaultdict(CountDict)
