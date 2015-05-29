4d18ec494ab733c542c97ebfe06366923d0d2467
Ryan Georgi <rgeorgi@gmail.com>
2015-05-29 00:29:13 -0500

    Update to extraction script, now does preprocessing to filter out instances not seen frequently enough.
    Former-commit-id: 487d861dc6d6e5a366cd097bed1a1eaf7c9ae5c6

--- intent/scripts/extraction.py
+++ intent/scripts/extraction.py
@@ -1,9 +1,10 @@
 import logging
 from tempfile import NamedTemporaryFile
+import sys
 from intent.igt.consts import POS_TIER_TYPE, GLOSS_WORD_ID
 from intent.igt.grams import write_gram
 from intent.interfaces.mallet_maxent import train_txt
-from intent.utils.token import GoldTagPOSToken
+from intent.utils.token import GoldTagPOSToken, tokenize_string, morpheme_tokenizer
 from xigt.consts import ALIGNMENT
 
 EXTRACT_LOG = logging.getLogger("EXTRACT")
@@ -24,18 +25,15 @@ def extract_from_xigt(input_filelist = list, classifier_prefix=None, cfg_path=No
     :param cfg_path:
     """
 
+    # ------- Dictionaries for keeping track of gloss_pos preprocessing. --------
+
     word_tag_dict = TwoLevelCountDict()
+    gram_tag_dict = TwoLevelCountDict()
 
-    # Let's save the temporary file that we use to create the
-    # classifier features...
-    if classifier_prefix is not None:
+    # ---------------------------------------------------------------------------
 
-        # The path for the svm-light-based features.
-        feat_path  =  classifier_prefix+'.feats.txt'
-        class_path  = classifier_prefix+'.classifier'
-
-        feat_file = open(feat_path, 'w', encoding='utf-8')
-        print('Writing svm-light style features out to "{}"'.format(feat_path))
+    if classifier_prefix:
+        print("Gathering statistics on POS tags...")
 
     # Start by loading each of the files in the input files...
     for path in input_filelist:
@@ -47,14 +45,106 @@ def extract_from_xigt(input_filelist = list, classifier_prefix=None, cfg_path=No
 
             # Only work with the gloss POS tags if we ask for them...
             if classifier_prefix is not None:
-                process_gloss_pos_line(inst, word_tag_dict, feat_file)
+                gather_gloss_pos_stats(inst, word_tag_dict, gram_tag_dict)
+
 
     # Finally, try training the classifier on this file...
     if classifier_prefix is not None:
+
+        # The path for the svm-light-based features.
+        feat_path  =  classifier_prefix+'.feats.txt'
+        class_path  = classifier_prefix+'.classifier'
+
+        write_out_gram_dict(word_tag_dict, feat_path)
+
         print("Training classifier... ", end='')
         train_txt(feat_path, class_path)
         print("Complete.")
-        feat_file.close()
+
+
+# =============================================================================
+# Preprocessing
+#
+# This section is for preprocessing the gloss pos tags by limiting the tags gathered
+# to only those subsets that occur frequently enough.
+# =============================================================================
+
+def gather_gloss_pos_stats(inst, word_tag_dict, gram_tag_dict):
+    """
+    Given an instance, look for the gloss pos tags, and save the statistics
+    about them, so that we can filter by the number of times each kind was
+    seen later.
+
+    :param inst: Instance to process.
+    :type inst: RGIgt
+    :param word_tag_dict: This dictionary will record the number of times each (word, TAG)
+                          pair has been seen.
+    :type word_tag_dict: TwoLevelCountDict
+    :param gram_tag_dict: This dictionary will record the number of times individual grams are seen.
+    :type gram_tag_dict: TwoLevelCountDict
+    """
+
+    # Grab the gloss POS tier...
+    gpos_tier = inst.find(alignment=GLOSS_WORD_ID, type=POS_TIER_TYPE)
+
+    # If this tier exists, then let's process it.
+    if gpos_tier is not None:
+
+        # Iterate over each gloss POS tag...
+        for gpos in gpos_tier:
+
+            # Skip this tag if for some reason it doesn't align with
+            # a gloss word.
+            if ALIGNMENT not in gpos.attributes or not gpos.alignment:
+                EXTRACT_LOG.debug("No alignment found for {} in tier {} igt {}".format(gpos.id, gpos.tier.id, gpos.igt.id))
+                continue
+
+            word = gpos.igt.find(id=gpos.alignment).value()
+            tag  = gpos.value()
+
+            word_tag_dict.add(word.lower(), tag)
+
+            # Now, let's split the word into its composite grams, and add those.
+            morphs = tokenize_string(word, tokenizer=morpheme_tokenizer)
+            for morph in morphs:
+                gram_tag_dict.add(morph.seq.lower(), tag)
+
+# =============================================================================
+# Postprocessing
+#
+# Now, after the grams/words have been counted up, let's filter out the grams/words
+# that have been seen enough to meet our threshold, and train the classifier.
+# =============================================================================
+
+def write_out_gram_dict(gram_tag_dict, feat_path, threshold = 3):
+    """
+    Given the gram+tag dict, write out grams for those that have been seen enough to
+    meet our threshold.
+
+    :param gram_tag_dict:
+    :type gram_tag_dict: TwoLevelCountDict
+    :param feat_path:
+    :param class_path:
+    """
+
+    print('Writing out svm-lite style features to "{}"...'.format(feat_path))
+    feat_file = open(feat_path, 'w', encoding='utf-8')
+
+    for gram in gram_tag_dict.keys():
+
+        # Only write out the gram if we've seen it at least as many
+        # times as in threshold.
+        if gram_tag_dict.total(gram) >= threshold:
+            for tag in gram_tag_dict[gram].keys():
+
+                # Write out the gram with this tag as many times as it appears...
+                for i in range(gram_tag_dict[gram][tag]):
+
+                    gt = GoldTagPOSToken(gram, goldlabel=tag)
+                    write_gram(gt, feat_next_gram=False, feat_prev_gram=False, lowercase=True, output=feat_file)
+    feat_file.close()
+
+
 
 
 def process_gloss_pos_line(inst, word_tag_dict, outfile):
