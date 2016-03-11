bad7168c8da153738b665b638cfb34be30668f5d
Ryan Georgi <rgeorgi@gmail.com>
2016-03-10 16:12:54 -0800

    Fixes to evaluating the classifier.

--- intent/commands/evaluation.py
+++ intent/commands/evaluation.py
@@ -8,9 +8,13 @@ import threading
 
 import time
 
+from nltk.tag import pos_tag_sents
+
 from intent.consts import NORM_LEVEL, ARG_ALN_MANUAL, INTENT_ALN_MANUAL, INTENT_ALN_GIZA, ALIGNER_FASTALIGN, \
     GLOSS_WORD_ID, LANG_WORD_ID, INTENT_ALN_HEUR, INTENT_POS_TAGGER, INTENT_POS_PROJ, INTENT_POS_CLASS, \
-    INTENT_ALN_GIZAHEUR, ALIGNER_GIZA, INTENT_DS_MANUAL, INTENT_DS_PARSER, INTENT_DS_PROJ, INTENT_ALN_HEURPOS
+    INTENT_ALN_GIZAHEUR, ALIGNER_GIZA, INTENT_DS_MANUAL, INTENT_DS_PARSER, INTENT_DS_PROJ, INTENT_ALN_HEURPOS, \
+    INTENT_POS_MANUAL
+from intent.igt.exceptions import GlossLangAlignException, RGXigtException
 from intent.igt.igtutils import rgp
 
 from intent.igt.parsing import xc_load
@@ -21,8 +25,9 @@ from intent.eval.pos_eval import poseval
 from intent.igt.igt_functions import heur_align_corp, giza_align_t_g, remove_alignments, copy_xigt, heur_align_inst, \
     classify_gloss_pos, tag_trans_pos, get_trans_gloss_lang_alignment, get_trans_gloss_alignment, giza_align_l_t, \
     get_trans_lang_alignment, get_bilingual_alignment_tier, add_gloss_lang_alignments, tier_alignment, get_lang_ds, \
-    parse_translation_line, project_ds_tier
-from intent.igt.create_tiers import trans, gloss, gloss_tag_tier, glosses
+    parse_translation_line, project_ds_tier, project_trans_pos_to_gloss, project_lang_to_gloss
+from intent.igt.create_tiers import trans, gloss, gloss_tag_tier, glosses, pos_tag_tier
+from intent.igt.references import xigt_find
 from intent.interfaces.mallet_maxent import MalletMaxent
 from intent.interfaces.stanford_tagger import StanfordPOSTagger
 from intent.trees import TreeProjectionError
@@ -76,7 +81,7 @@ def evaluate_intent(filelist, classifier_path=None, eval_alignment=None, eval_ds
         # Test the classifier if evaluation is requested.
         # -------------------------------------------
         if classifier_path is not None:
-            prj_eval, cls_eval = evaluate_classifier_on_instances(xc, classifier, tagger)
+            prj_eval, cls_eval = evaluate_classifier_on_instances(xc, classifier_obj, tagger)
 
             overall_prj += prj_eval
             overall_cls += cls_eval
@@ -109,7 +114,7 @@ def evaluate_intent(filelist, classifier_path=None, eval_alignment=None, eval_ds
     # Report the POS tagging accuracy...
     if classifier_path is not None:
         print("ALL...")
-        print('{:.2f},{:.2f}'.format(overall_prj.accuracy(), overall_cls.accuracy()))
+        print('{:.2f},{:.2f},{:.2f}'.format(overall_prj.accuracy(), overall_prj.unaligned(), overall_cls.accuracy()))
 
 
 
@@ -396,49 +401,55 @@ def evaluate_instance(inst, classifier, tagger):
     :param classifier: MalletMaxent
     :param tagger: StanfordPOSTagger
     """
-    sup_gloss_tier = inst.get_pos_tags(GLOSS_WORD_ID)  # We will incrementally build up the tag sequences...
-    sup_lang_tier  = inst.get_pos_tags(LANG_WORD_ID)
+    sup_gloss_tier = pos_tag_tier(inst, GLOSS_WORD_ID)  # We will incrementally build up the tag sequences...
+    sup_lang_tier  = pos_tag_tier(inst, LANG_WORD_ID)
+
+
     sup_tags = []
     prj_tags = []
     cls_tags = []
 
     # If there are no supervised tags on the gloss line, but there are on the language line...
     if sup_gloss_tier is None and sup_lang_tier is not None:
-        inst.add_gloss_lang_alignments()
-        inst.project_lang_to_gloss()
-        sup_gloss_tier = inst.get_pos_tags(GLOSS_WORD_ID)
+        try:
+            add_gloss_lang_alignments(inst)
+            project_lang_to_gloss(inst)
+            sup_gloss_tier = pos_tag_tier(inst, GLOSS_WORD_ID)
+        except RGXigtException:
+            pass
 
     if sup_gloss_tier:
 
         # Do the classification
-        inst.classify_gloss_pos(classifier)
+        classify_gloss_pos(inst, classifier)
 
         # Do the projection...
-        inst.heur_align()
-        inst.tag_trans_pos(tagger)
+        heur_align_inst(inst)
 
-        inst.project_trans_to_gloss(aln_method=INTENT_ALN_HEUR, tag_source=INTENT_POS_TAGGER)
+        tag_trans_pos(inst, tagger)
+
+        project_trans_pos_to_gloss(inst, aln_method=INTENT_ALN_HEUR, trans_tag_method=INTENT_POS_TAGGER)
 
         # Now, go through each aligned ID for the supervised tags, and match them with those in the other
         # tiers... IF they exist.
 
-        prj_tier = inst.get_pos_tags(GLOSS_WORD_ID, tag_method=INTENT_POS_PROJ)
-        cls_tier = inst.get_pos_tags(GLOSS_WORD_ID, tag_method=INTENT_POS_CLASS)
+        prj_tier = pos_tag_tier(inst, GLOSS_WORD_ID, tag_method=INTENT_POS_PROJ)
+        cls_tier = pos_tag_tier(inst, GLOSS_WORD_ID, tag_method=INTENT_POS_CLASS)
 
         for sup_item in sup_gloss_tier:
-            word = inst.find(id=sup_item.alignment)
+            word = xigt_find(inst, id=sup_item.alignment)
             if not word:
                 continue
             else:
                 word = word.value()
 
-            prj_item = prj_tier.find(alignment=sup_item.alignment)
+            prj_item = xigt_find(prj_tier, alignment=sup_item.alignment)
             if prj_item is None:
                 prj_tag = 'UNK'
             else:
                 prj_tag = prj_item.value()
 
-            cls_item = cls_tier.find(alignment=sup_item.alignment)
+            cls_item = xigt_find(cls_tier, alignment=sup_item.alignment)
             if cls_item is None:
                 cls_tag = 'UNK'
             else:
@@ -463,7 +474,7 @@ def evaluate_classifier_on_instances(inst_list, classifier, tagger):
     sup_sents, prj_sents, cls_sents = [], [], []
 
     for inst in inst_list:
-        sup_tags, prj_tags, cls_tags = evaluate_instance(inst.copy(), classifier, tagger)
+        sup_tags, prj_tags, cls_tags = evaluate_instance(copy_xigt(inst), classifier, tagger)
 
         sup_sents.append(sup_tags)
         prj_sents.append(prj_tags)
