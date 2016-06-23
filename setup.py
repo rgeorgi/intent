711f874685352b2600629def5eff92c6f8eb7a7b
Ryan Georgi <rgeorgi@gmail.com>
2016-06-23 16:44:00 -0700

    Fixing setup.py for 1.0 version.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    install_requires=['xigt>=1.0', 'nltk>=3.0.0', 'lxml>=3.4.1'],
+    install_requires=['xigt>=1.0', 'nltk>=3.0.0', 'lxml>=3.4.1', 'nose>=1.3.0'],
     dependency_links=[
         "git+ssh://git@github.com:goodmami/xigt.git"
     ],
-    version='0.4',
+    version='1.0',
     description='INterlinear Text ENrichment Toolkit',
     author_email='rgeorgi@uw.edu',
     url='https://github.com/rgeorgi/intent',
